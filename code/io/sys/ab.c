00469fb2214ea7b9ce73bcb9df40776a82b93e23
liangruuu <1024311844@qq.com>
2022-03-20 10:16:15 +0800

    push

---
+++ code/io/sys/ab.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(void)
+{
+
+    putchar('a');
+    write(1, "b", 1);
+
+    putchar('a');
+    write(1, "b", 1);
+
+    putchar('a');
+    write(1, "b", 1);
+
+    return 0;
+}
